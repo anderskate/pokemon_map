3ae6ecac3cc44eefdf91772671b73557e10b1ba4
anderskate <46349173+anderskate@users.noreply.github.com>
2019-08-05 22:39:29 +0500

    added to the required fields null=True

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -5,7 +5,7 @@ class Pokemon(models.Model):
     title = models.CharField('название', max_length=200)
     title_en = models.CharField('название на англ.', max_length=200, blank=True)
     title_jp = models.CharField('название на яп.', max_length=200, blank=True)
-    image = models.ImageField('изображение', blank=True)
+    image = models.ImageField('изображение', blank=True, null=True)
     description = models.TextField('описание', blank=True)
     next_evolution = models.ForeignKey(
         'self', 
@@ -29,15 +29,15 @@ class Pokemon(models.Model):
 
 class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, verbose_name='покемон', on_delete=models.CASCADE)
-    lat = models.FloatField('широта', blank=True)
-    lon = models.FloatField('долгота', blank=True)
-    appeared_at = models.DateTimeField('когда появится', blank=True)
-    disappeared_at = models.DateTimeField('когда исчезнет', blank=True)
-    level = models.IntegerField('уровень', blank=True)
-    health = models.IntegerField('здоровье', blank=True)
-    strength = models.IntegerField('сила', blank=True)
-    defence = models.IntegerField('защита', blank=True)
-    stamina = models.IntegerField('выносливость', blank=True)
+    lat = models.FloatField('широта', blank=True, null=True)
+    lon = models.FloatField('долгота', blank=True, null=True)
+    appeared_at = models.DateTimeField('когда появится', blank=True, null=True)
+    disappeared_at = models.DateTimeField('когда исчезнет', blank=True, null=True)
+    level = models.IntegerField('уровень', blank=True, null=True)
+    health = models.IntegerField('здоровье', blank=True, null=True)
+    strength = models.IntegerField('сила', blank=True, null=True)
+    defence = models.IntegerField('защита', blank=True, null=True)
+    stamina = models.IntegerField('выносливость', blank=True, null=True)
 
     def __str__(self):
         return "{entity_id} cущность покемона - '{pokemon}'".format(
